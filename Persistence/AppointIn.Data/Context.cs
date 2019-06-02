75cfcf248dd21eea7c7a770e383fc8f65c54dfc1
Francisco Javier Becerra-Ortiz <francisco@fjbo.net>
2019-06-01 21:10:16 -0700

    Initial Entitity Framework setup.

---
+++ Persistence/AppointIn.Data/Context.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using AppointIn.Domain.Entities;
+
+namespace AppointIn.Data
+{
+	public class Context:DbContext
+	{
+		public Context()
+		{
+			Database.SetInitializer(new DropCreateDatabaseIfModelChanges<Context>());
+		}
+
+		public DbSet<Address> Addresses { get; set; }
+		public DbSet<Appointment> Appointments { get; set; }
+		public DbSet<City> Cities { get; set; }
+		public DbSet<Country> Countries { get; set; }
+		public DbSet<Customer> Customers { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
